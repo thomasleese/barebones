9f4a6eb265b12bd1569800929e2fef6bf09bb9a4
Tom Leese <inbox@tomleese.me.uk>
2014-12-21 11:19:27 +0000

    Update metadata gumpf

---
+++ README.rst
@@ -0,0 +1,18 @@
+Barebones
+=========
+
+Interpreter for an extended Barebones language written in Python.
+
+Installation
+------------
+
+.. code::
+
+    $ ./setup.py install
+
+Usage
+-----
+
+.. code::
+
+    $ barebones examples/*
--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
 from setuptools import setup
 
+
 setup(
     name="barebones",
-    version="0.0.0",
+    version="1.0.0",
     description="Interpreter for an extended Barebones language written in Python.",
     url="https://github.com/tomleese/barebones",
     author="Tom Leese",
-    author_email="tom@tomleese.me.uk",
+    author_email="inbox@tomleese.me.uk",
     packages=["barebones"],
     entry_points = {
         "console_scripts": ["barebones = barebones:main"]
